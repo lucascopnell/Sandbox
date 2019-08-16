cab57bdb808581338d22d17c743cc6e0dbde84d6
Lucas <lucas.copnell@my.jcu.edu.au>
2019-08-16 15:55:22 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,10 @@
 """Lucas Copnell"""
+
+MIN_LENGTH = 3
+MAX_LENGTH = 10
+
+
+password = input("Please enter a Password: ")
+while len(password) < 3 or len(password) > 10:
+    password = input("Please enter a Password: ")
+print(len(password)*"*")
